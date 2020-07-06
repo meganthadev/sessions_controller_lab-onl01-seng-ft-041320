f2c56fa2712c1ab8775238e3391d12de189142ec
Megan Horsfield <meganthadev@gmail.com>
2020-07-06 08:04:05 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if session[:name] = params[:name]
      redirect_to '/'
    else
-     redirect_to '/'
+     redirect_to '/login'
   end  
 end 
   
